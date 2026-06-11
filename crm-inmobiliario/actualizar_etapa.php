1d2ee3b09871e3d2b00caa70a7021ead912fc68e
ScaiderGod <119545036+ScaiderGod@users.noreply.github.com>
2026-06-11 10:43:32 -0600

    Delete crm-inmobiliario directory

--- crm-inmobiliario/actualizar_etapa.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) exit;
-require_once 'config.php';
-if ($_POST['id'] && $_POST['etapa']) {
-    $stmt = $pdo->prepare("UPDATE oportunidades SET etapa = ? WHERE id = ? AND user_id = ?");
-    $stmt->execute([$_POST['etapa'], $_POST['id'], $_SESSION['user_id']]);
-}
-?>
